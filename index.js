05d0e85b7285e8fd73dbc4607870e0755e8890ee
PogorelovEY <jeckcame@gmail.com>
2021-11-11 14:09:50 +0300

    Начало

---
+++ index.js
@@ -0,0 +1,8 @@
+// console.log(process.argv);
+// const name = process.argv[2];
+// console.log("Hello from Node.js to", name);
+
+const colors = require('colors');
+
+console.log(colors.green('Hellow'));
+console.log('test');
