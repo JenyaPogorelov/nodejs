c70bc0a7a360958d13b25102720bed13570e1f69
PogorelovEY <jeckcame@gmail.com>
2021-11-18 16:52:00 +0300

    Чистка в коде

--- index.js
+++ index.js
@@ -24,30 +24,36 @@ require('moment-precise-range-plugin');
 
 moment.locale("ru")
 
+let dateReg = /^(19|20)[0-9]{2}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])-([01]?[1-9]|2[0-3])-([0-5]?[0-9])-([0-5]?[0-9])$/
+
 if (!(process.argv.length > 2)) {
     console.log('Введите данные')
     return;
-}
-
-let dateReg = /^(19|20)[0-9]{2}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])-([01]?[1-9]|2[0-3])-([0-5]?[1-9])-([0-5]?[0-9])$/
-let time = process.argv[2];
-
-if (!time.match(dateReg)) {
-    console.log("Неверный формат даты, Верный формат:", colors.red("год-месяц-день-час-минута-секунда"));
 } else {
-    let timeThen = moment(time, 'YYYY M D h mm ss').format('YYYY-M-D hh:mm:ss');
-    let timer = setInterval(() => {
-        let timeNow = moment().format('YYYY-M-D hh:mm:ss')
-        let diff = moment.preciseDiff(timeThen, timeNow);
+    process.argv.forEach(async (element, index) =>  {
+        if (index > 1) {
+            let time = process.argv[index];
 
-        if (timeThen <= timeNow) {
-            console.clear();
-            console.log("Время вышло")
-            clearInterval(timer);
-        } else {
-            console.clear();
-            console.log(diff);
+            if (!time.match(dateReg)) {
+                console.log("Неверный формат даты, Верный формат:", colors.red("год-месяц-день-час-минута-секунда"));
+            } else {
+                let timeThen = moment(time, 'YYYY M D h mm ss').format('YYYY-M-D hh:mm:ss');
+                let timer = await setInterval(() => {
+                    let timeNow = moment().format('YYYY-M-D hh:mm:ss')
+                    let diff = moment.preciseDiff(timeThen, timeNow);
+
+                    if (timeThen <= timeNow) {
+                        console.clear();
+                        console.log("Время вышло")
+                        clearInterval(timer);
+                    } else {
+                        console.clear();
+                        console.log(diff);
+                    }
+                }, 1000)
+            }
         }
-    }, 1000)
+    })
 }
+
 
