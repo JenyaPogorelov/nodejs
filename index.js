9d2ea29a903036fb4b2e0975d5ef287817b28d86
PogorelovEY <jeckcame@gmail.com>
2021-11-18 15:35:21 +0300

    Чистка в коде

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 //
 // Promise.resolve().then(() => Promise.resolve().then(() => console.log('Record 6')));
 //
-// // 1 5 6 2 3 4
+// // Ответ: 1 5 6 2 3 4
 
 const colors = require('colors');
 const moment = require('moment');
@@ -29,31 +29,25 @@ if (!(process.argv.length > 2)) {
     return;
 }
 
-// let dateReg = /^([0-5]?[0-9])-([0-5]?[1-9])-([01]?[1-9]|2[0-3])-(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|1[012])-(19|20)[0-9]{2}$/
 let dateReg = /^(19|20)[0-9]{2}-(0?[1-9]|1[012])-(0?[1-9]|[12][0-9]|3[01])-([01]?[1-9]|2[0-3])-([0-5]?[1-9])-([0-5]?[0-9])$/
 let time = process.argv[2];
-
-let timeThen = moment(time, 'YYYY M D h mm ss').format('YYYY-M-D hh:mm:ss');
-
-let timer = setInterval(() => {
-    let timeNow = moment().format('YYYY-M-D hh:mm:ss')
-    let diff = moment.preciseDiff(timeThen, timeNow);
-
-    if (timeThen <= timeNow) {
-        console.clear();
-        console.log("Время вышло")
-        clearInterval(timer);
-    } else {
-        console.clear();
-        console.log(diff);
-    }
-},1000 )
-
 
 if (!time.match(dateReg)) {
     console.log("Неверный формат даты, Верный формат:", colors.red("год-месяц-день-час-минута-секунда"));
 } else {
+    let timeThen = moment(time, 'YYYY M D h mm ss').format('YYYY-M-D hh:mm:ss');
+    let timer = setInterval(() => {
+        let timeNow = moment().format('YYYY-M-D hh:mm:ss')
+        let diff = moment.preciseDiff(timeThen, timeNow);
 
-
+        if (timeThen <= timeNow) {
+            console.clear();
+            console.log("Время вышло")
+            clearInterval(timer);
+        } else {
+            console.clear();
+            console.log(diff);
+        }
+    }, 1000)
 }
 
