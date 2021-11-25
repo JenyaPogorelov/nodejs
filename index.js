6238fe9fc1c321ca3ab89eeb20a23d1142f32376
ShadowD <jeckcame@gmail.com>
2021-11-25 14:35:21 +0300

    Сделал 4е ДЗ

--- index.js
+++ index.js
@@ -5,16 +5,34 @@ const path = require('path');
 const readline = require('readline');
 const inquirer = require('inquirer');
 
+// const directoryPath = process.argv[2];
+// const whatFind = process.argv[3];
+
+// const childProcess = require('child_process');
+// const {log} = require("yarn/lib/cli");
+// function getLocalDiskNames() {
+//     const buffer = childProcess.execSync('wmic logicaldisk get Caption  /format:list').toString();
+//     const lines = buffer.split('\r\r\n');
+//     const disks = [];
+//
+//     for (const line of lines) {
+//         if(!line) {
+//             continue;
+//         }
+//
+//         const lineData = line.split('=');
+//         disks.push(lineData[1]);
+//     }
+//
+//     return disks;
+// }
 
 let executionDir = process.cwd();
 const disk = executionDir.slice(0, 3);
 const isFile = (filename) => fs.lstatSync(filename).isFile();
-let list = fs.readdirSync(executionDir).sort((filename) => {
-    if (isFile(filename)) return 1;
-    return -1;
-});
 
-let fileManager = (async (list) => {
+let fileManager = ((list, whatFind) => {
+    console.log('whatFind', whatFind);
     inquirer.prompt([
         {
             name: 'fileName',
@@ -25,8 +43,14 @@ let fileManager = (async (list) => {
     ]).then(({fileName}) => {
         if (isFile(executionDir + `\\${fileName}`)) {
             const fullFilePath = path.resolve(executionDir, fileName);
-            const data = fs.readFileSync(fullFilePath, 'utf-8');
-            console.log(data);
+            // const data = fs.readFileSync(fullFilePath, 'utf-8');
+            // console.log(data);
+            const lineReader = readline.createInterface({
+                input: fs.createReadStream(fullFilePath)
+            });
+            lineReader.on("line", (input) => {
+                if (input.lastIndexOf(whatFind) !== -1) console.log(input);
+            })
         } else {
             if (fileName === '..') {
                 executionDir = disk + executionDir.slice(3, executionDir.lastIndexOf('\\'));
@@ -43,4 +67,20 @@ let fileManager = (async (list) => {
     });
 })
 
-fileManager(list);
+inquirer.prompt([
+    {
+        name: 'directoryPath',
+        type: 'input', //input, number, confirm, list, checkbox, password
+        message: 'Select Dir (Диск:\\полный путь до файла): ',
+    },
+    {
+        name: 'whatFind',
+        type: 'input', //input, number, confirm, list, checkbox, password
+        message: 'what find in file: ',
+    },
+]).then(({directoryPath, whatFind}) => {
+    if (directoryPath) executionDir = directoryPath
+    let list = fs.readdirSync(executionDir);
+    fileManager(list, whatFind);
+})
+
