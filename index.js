757d825154a95763caf3f4ae5bf93e3efd293f54
ShadowD <jeckcame@gmail.com>
2021-12-01 21:17:59 +0300

    6 ДЗ...

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-// console.log(process.argv);
-// const name = process.argv[2];
-// console.log("Hello from Node.js to", name);
+const socket = require('socket.io');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const colors = require('colors');
+const server = http.createServer((req, res) => {
+    const indexPath = path.join(__dirname, 'index.html');
+    const readStream = fs.createReadStream(indexPath);
 
-console.log(colors.green('Hellow'));
-console.log('test');
+    readStream.pipe(res);
+});
+
+const io = socket(server);
+
+io.on('connection', client => { //connect, disconnect, reconnect
+    console.log(`client ${ client.id } connected`);
+
+    client.broadcast.emit('server-connectedID', client.id);
+
+    client.on('client-msg', data => {
+        // console.log(data);
+        const payload = {
+            // message: data.message.split('').reverse().join(''),
+            message: data.message,
+            id: client.id,
+        };
+        client.broadcast.emit('server-msg', payload);
+        client.emit('server-msg', payload);
+    });
+    client.on('disconnect', data => {
+        const disconnectedID = {
+            id: client.id,
+        }
+        client.broadcast.emit('server-disconnectedID', disconnectedID);
+        client.emit('server-disconnectedID', disconnectedID);
+    });
+    client.on('reconnect', data => {
+        const reconnectedID = {
+            id: client.id,
+        }
+        console.log('reconnected')
+        client.broadcast.emit('server-reconnectedID', reconnectedID);
+        client.emit('server-reconnectedID', reconnectedID);
+    });
+});
+// connected, data, client, disconnect, reconnect, sids
+
+
+server.listen(5555);
